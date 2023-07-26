f39ae8bb79825e648d628ed75f03f9dfe8b6e3f7
ryeubi <iyahn@keti.re.kr>
2023-07-26 10:09:15 +0900

    - 파일 구조 조정 중

--- tilt_motor.js
+++ tilt_motor.js
@@ -76,7 +76,6 @@ let target_relative_altitude = '';
 
 let motor_return_msg = '';
 
-let sub_drone_data_topic = '/RF/TELE_HUB/drone';
 let sub_motor_control_topic = '/Ant_Tracker/Control';
 let sub_motor_altitude_topic = '/Ant_Tracker/Altitude';
 //let sub_gps_location_topic = '/GPS/location';
@@ -171,9 +170,6 @@ function localMqttConnect(host) {
     localmqtt = mqtt.connect(connectOptions);
 
     localmqtt.on('connect', function () {
-        localmqtt.subscribe(sub_drone_data_topic + '/#', () => {
-            console.log('[tilt] localmqtt subscribed -> ', sub_drone_data_topic);
-        });
         //localmqtt.subscribe(sub_gps_location_topic + '/#', () => {
         //    console.log('[tilt] localmqtt subscribed -> ', sub_gps_location_topic);
         //});
@@ -196,90 +192,13 @@ function localMqttConnect(host) {
         if (topic == sub_motor_control_topic) { // 모터 제어 메세지 수신
             motor_control_message = message.toString();
             // console.log(topic, motor_control_message);
-        } else if (topic == sub_motor_altitude_topic) {
+        }
+        else if (topic == sub_motor_altitude_topic) {
             motor_altitude_message = message.toString();
             if (typeof (parseInt(motor_altitude_message)) === 'number') {
                 myRelativeAltitude = motor_altitude_message;
             }
-        } else if (topic.includes(sub_drone_data_topic)) { // 드론데이터 수신
-            localmqtt_message = message.toString('hex');
-            // console.log("Client1 topic => " + topic);
-            // console.log("Client1 message => " + drone_message);
-
-            try {
-                let ver = localmqtt_message.substring(0, 2);
-                let sysid = '';
-                let msgid = '';
-                let base_offset = 0;
-
-                if (ver == 'fd') {//MAV ver.1
-                    sysid = localmqtt_message.substring(10, 12).toLowerCase();
-                    msgid = localmqtt_message.substring(18, 20) + localmqtt_message.substring(16, 18) + localmqtt_message.substring(14, 16);
-                    base_offset = 20;
-                } else { //MAV ver.2
-                    sysid = localmqtt_message.substring(6, 8).toLowerCase();
-                    msgid = localmqtt_message.substring(10, 12).toLowerCase();
-                    base_offset = 12;
-                }
-
-                let sys_id = parseInt(sysid, 16);
-                let msg_id = parseInt(msgid, 16);
-
-                if (msg_id === 33) { // MAVLINK_MSG_ID_GLOBAL_POSITION_INT
-                    var time_boot_ms = localmqtt_message.substring(base_offset, base_offset + 8).toLowerCase()
-                    base_offset += 8
-                    let lat = localmqtt_message.substring(base_offset, base_offset + 8).toLowerCase().toString();
-                    base_offset += 8;
-                    let lon = localmqtt_message.substring(base_offset, base_offset + 8).toLowerCase();
-                    base_offset += 8;
-                    let alt = localmqtt_message.substring(base_offset, base_offset + 8).toLowerCase();
-                    base_offset += 8;
-                    let relative_alt = localmqtt_message.substring(base_offset, base_offset + 8).toLowerCase();
-
-                    target_latitude = Buffer.from(lat, 'hex').readInt32LE(0).toString() / 10000000;
-                    target_longitude = Buffer.from(lon, 'hex').readInt32LE(0).toString() / 10000000;
-                    target_altitude = Buffer.from(alt, 'hex').readInt32LE(0).toString() / 1000;
-                    target_relative_altitude = Buffer.from(relative_alt, 'hex').readInt32LE(0).toString() / 1000;
-
-                    // console.log('target_latitude, target_longitude, target_altitude, target_relative_altitude', target_latitude, target_longitude, target_altitude, target_relative_altitude);
-                    
-                    if(run_flag === 'go') {
-						target_angle = calcTargetTiltAngle(target_latitude, target_longitude, target_relative_altitude);
-						// console.log('myPitch, target_angle', myPitch, target_angle);
-
-						if (Math.abs(target_angle - myPitch) > 10) {
-							p_step = 0.02;
-						} else if (Math.abs(target_angle - myPitch) > 5) {
-							p_step = 0.01;
-						} else if (Math.abs(target_angle - myPitch) > 3) {
-							p_step = 0.005;
-						} else {
-							p_step = 0.001;
-						}
-
-						if (myPitch !== target_angle) {
-							cw = target_angle - myPitch;
-							if (cw < 0) {
-								cw = cw + 360;
-							}
-							ccw = 360 - cw;
-
-							if (cw < ccw) {
-								p_in = p_in + p_step;
-							} else if (cw > ccw) {
-								p_in = p_in - p_step;
-							} else {
-								p_in = p_in;
-							}
-						}
-						p_step = 0.02;
-					}
-                }
-            }
-            catch (e) {
-                console.log('[tilt] local mqtt connect Error', e);
-            }
-        } 
+        }
         //else if (topic === sub_gps_location_topic) { // 픽스호크로부터 받아오는 트래커 위치 좌표
         //    tracker_location_msg = JSON.parse(message.toString());
             //myLatitude = tracker_location_msg.lat;
