444c1a902be68cad35215dfd36b1e9768fffa6d6
dnjstjr93 <dnjstjr93@gmail.com>
2023-08-21 21:47:09 +0900

    turnTarget 함수 업데이트

--- motor_can.js
+++ motor_can.js
@@ -341,7 +341,7 @@ let V = () => {
 }
 
 let turn_flag = 0;
-const big_gap = 1.8 * 0.0174533;
+const big_gap = 2.7 * 0.0174533;
 const small_gap = 0.9 * 0.0174533;
 
 let turnTarget = (_in, _target) => {
