c4a1c5d1f91c2b3d43099930f1835dbfdf0085ed
dnjstjr93 <dnjstjr93@gmail.com>
2023-08-21 16:10:00 +0900

    commMotor 함수 인수 추가

--- motor_can.js
+++ motor_can.js
@@ -194,9 +194,9 @@ let commMotor = (_in, _target) => {
                 setTimeout(commMotor, 50, _in);
             });
         }
-        // else {
-        //     setTimeout(commMotor, 100, _in);
-        // }
+        else {
+            setTimeout(commMotor, 100, _in);
+        }
     }
     else if(stateMotor === 'toZero') {
         Zero(() => {
