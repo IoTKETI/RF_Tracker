db588001393e617ae6ddcdd42337333377971df3
dnjstjr93 <dnjstjr93@gmail.com>
2023-08-30 17:24:08 +0900

    드론 트랜커 구조 renewal

--- tr_ctrl.js
+++ tr_ctrl.js
@@ -191,14 +191,96 @@ function getDistance(lat1, lon1, lat2, lon2) {
 }
 
 function calcTargetTiltAngle(targetLatitude, targetLongitude, targetAltitude) {
-    let x = getDistance(tracker_latitude, tracker_longitude, targetLatitude, targetLongitude);
+    //let x = getDistance(tracker_latitude, tracker_longitude, targetLatitude, targetLongitude);
+
+    let cur_lat = tracker_latitude;
+    let cur_lon = tracker_longitude;
+    let cur_alt = tracker_altitude;
+
+    let result1 = dfs_xy_conv('toXY', cur_lat, cur_lon);
+
+    let tar_lat = targetLatitude;
+    let tar_lon = targetLongitude;
+    let tar_alt = targetAltitude;
+
+    let result2 = dfs_xy_conv('toXY', tar_lat, tar_lon);
+
+    let x = Math.sqrt(Math.pow(result2.x - result1.x, 2) + Math.pow(result2.y - result1.y, 2) + Math.pow((tar_alt - cur_alt), 2));
+
     let y = targetAltitude - tracker_altitude;
 
+    console.log(cur_lat, cur_lon, cur_alt);
+    console.log(tar_lat, tar_lon, tar_alt);
     console.log('x: ', x, '     y: ', y);
 
     let angle = Math.atan2(y, x);
 
     return Math.round(angle * 180 / Math.PI);
+}
+
+const RE = 6371.00877; // 지구 반경(km)
+const GRID = 0.001; // 격자 간격(km)
+const SLAT1 = 30.0; // 투영 위도1(degree)
+const SLAT2 = 60.0; // 투영 위도2(degree)
+const OLON = 126.0; // 기준점 경도(degree)
+const OLAT = 38.0; // 기준점 위도(degree)
+const XO = 43; // 기준점 X좌표(GRID)
+const YO = 136; // 기1준점 Y좌표(GRID)
+
+function dfs_xy_conv(code, v1, v2) {
+    const DEGRAD = Math.PI / 180.0;
+    const RADDEG = 180.0 / Math.PI;
+
+    const re = RE / GRID;
+    const slat1 = SLAT1 * DEGRAD;
+    const slat2 = SLAT2 * DEGRAD;
+    const olon = OLON * DEGRAD;
+    const olat = OLAT * DEGRAD;
+
+    let sn = Math.tan(Math.PI * 0.25 + slat2 * 0.5) / Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+    sn = Math.log(Math.cos(slat1) / Math.cos(slat2)) / Math.log(sn);
+    let sf = Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+    sf = Math.pow(sf, sn) * Math.cos(slat1) / sn;
+    var ro = Math.tan(Math.PI * 0.25 + olat * 0.5);
+    ro = re * sf / Math.pow(ro, sn);
+    let rs = {};
+    if (code === "toXY") {
+        rs['lat'] = v1;
+        rs['lng'] = v2;
+        var ra = Math.tan(Math.PI * 0.25 + (v1) * DEGRAD * 0.5);
+        ra = re * sf / Math.pow(ra, sn);
+        var theta = v2 * DEGRAD - olon;
+        if (theta > Math.PI) theta -= 2.0 * Math.PI;
+        if (theta < -Math.PI) theta += 2.0 * Math.PI;
+        theta *= sn;
+        rs['x'] = Math.floor(ra * Math.sin(theta) + XO + 0.5);
+        rs['y'] = Math.floor(ro - ra * Math.cos(theta) + YO + 0.5);
+    }
+    else {
+        rs['x'] = v1;
+        rs['y'] = v2;
+        let xn = v1 - XO;
+        let yn = ro - v2 + YO;
+        ra = Math.sqrt(xn * xn + yn * yn);
+        if (sn < 0.0) -ra;
+        let alat = Math.pow((re * sf / ra), (1.0 / sn));
+        alat = 2.0 * Math.atan(alat) - Math.PI * 0.5;
+
+        if (Math.abs(xn) <= 0.0) {
+            theta = 0.0;
+        }
+        else {
+            if (Math.abs(yn) <= 0.0) {
+                theta = Math.PI * 0.5;
+                if (xn < 0.0) -theta;
+            }
+            else theta = Math.atan2(xn, yn);
+        }
+        let alon = theta / sn + olon;
+        rs['lat'] = alat * RADDEG;
+        rs['lng'] = alon * RADDEG;
+    }
+    return rs;
 }
 
 
