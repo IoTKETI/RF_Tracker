abded5de9c357f8ab8b7855d31c8baae3468a721
gbsmfather <iyahn@keti.re.kr>
2023-10-14 02:30:03 +0900

    Simple BGC를 사용하여 SBUS로 트래커 조종하는 코드 추가 (작업중)

---
+++ motor_bgc.js
@@ -0,0 +1,218 @@
+const {SerialPort} = require('serialport');
+
+let sbus1Port = null;
+let sbus1PortNum = '/dev/ttyAMA3';
+let sbus1Baudrate = 100000;
+
+let sequence = 0;
+
+const RC_LENGTH = 68;
+const CH_SCALE = 8;
+
+let SBUS1_CH = new Uint16Array([1024, 1024, 1024, 1024, 1024, 223, 223, 1024, 223, 223, 223, 1024, 223, 223, 223, 223, 223, 223]);
+
+const crc8_Table = [
+    0, 94, 188, 226, 97, 63, 221, 131, 194, 156, 126, 32, 163, 253, 31, 65,  // 0 ~ 15
+    157, 195, 33, 127, 252, 162, 64, 30, 95, 1, 227, 189, 62, 96, 130, 220,  // 16 ~ 31
+    35, 125, 159, 193, 66, 28, 254, 160, 225, 191, 93, 3, 128, 222, 60, 98,  // 32 ~ 47
+    190, 224, 2, 92, 223, 129, 99, 61, 124, 34, 192, 158, 29, 67, 161, 255,		// 48 ~ 63
+    70, 24, 250, 164, 39, 121, 155, 197, 132, 218, 56, 102, 229, 187, 89, 7,  // 64 ~ 79
+    219, 133, 103, 57, 186, 228, 6, 88, 25, 71, 165, 251, 120, 38, 196, 154,  // 80 ~ 95
+    101, 59, 217, 135, 4, 90, 184, 230, 167, 249, 27, 69, 198, 152, 122, 36,   // 96 ~ 111
+    248, 166, 68, 26, 153, 199, 37, 123, 58, 100, 134, 216, 91, 5, 231, 185,  // 112 ~ 127
+    140, 210, 48, 110, 237, 179, 81, 15, 78, 16, 242, 172, 47, 113, 147, 205,  // 128 ~ 143
+    17, 79, 173, 243, 112, 46, 204, 146, 211, 141, 111, 49, 178, 236, 14, 80,  // 144 ~ 159
+    175, 241, 19, 77, 206, 144, 114, 44, 109, 51, 209, 143, 12, 82, 176, 238,  // 160 ~ 175
+    50, 108, 142, 208, 83, 13, 239, 177, 240, 174, 76, 18, 145, 207, 45, 115,  // 176 ~ 191
+    202, 148, 118, 40, 171, 245, 23, 73, 8, 86, 180, 234, 105, 55, 213, 139, // 192 ~ 207
+    87, 9, 235, 181, 54, 104, 138, 212, 149, 203, 41, 119, 244, 170, 72, 22,  // 208 ~ 223
+    233, 183, 85, 11, 136, 214, 52, 106, 43, 117, 151, 201, 74, 20, 246, 168,  // 224 ~ 239
+    116, 42, 200, 150, 21, 75, 169, 247, 182, 232, 10, 84, 215, 137, 107, 53  // 240 ~ 255
+];
+
+let sbus1PortOpening = () => {
+    if (!sbus1Port) {
+        sbus1Port = new SerialPort({
+            path: sbus1PortNum,
+            baudRate: parseInt(sbus1Baudrate, 10),
+            parity: 'even'
+        });
+
+        sbus1Port.on('open', () => {
+            console.log('sbus1Port(' + sbus1Port.path + '), sbus1Port rate: ' + sbus1Port.baudRate + ' open.');
+        });
+
+        sbus1Port.on('close', () => {
+            console.log('sbus1Port closed.');
+
+            setTimeout(sbus1PortOpening, 2000);
+        });
+
+        sbus1Port.on('error', () => {
+            console.log('[sbus1Port error]: ' + error.message);
+
+            setTimeout(sbus1PortOpening, 2000);
+        });
+
+        sbus1Port.on('data', (data) => {
+            let _resData = data.toString('hex').toLowerCase();
+
+            _resData = (sequence.toString(16).padStart(2, '0')) + _resData;
+
+            if (dr_mqtt_client) {
+                dr_mqtt_client.publish(res_topic, Buffer.from(_resData, 'hex'));
+            }
+            if (mobius_mqtt_client) {
+                mobius_mqtt_client.publish(res_topic, Buffer.from(_resData, 'hex'));
+            }
+
+            sequence++;
+            sequence %= 255;
+        });
+    }
+    else {
+        if (sbus1Port.isOpen) {
+            sbus1Port.close();
+            sbus1Port = null;
+            setTimeout(sbus1PortOpening, 2000);
+        }
+        else {
+            sbus1Port.open();
+        }
+    }
+}
+
+let Calc_CRC_8 = (DataArray, Length) => {
+    let i;
+    let crc;
+
+    crc = 0x01;
+    DataArray = Buffer.from(DataArray, 'hex');
+    for (i = 1; i < Length; i++) {
+        crc = crc8_Table[crc ^ DataArray[i]];
+    }
+    return crc;
+}
+
+const CH_VAL_MAX = 225;
+const CH_VAL_MID = 125;
+const CH_VAL_MIDMIN = 56;
+const CH_VAL_MIN = 25;
+
+let sbus_ch_val = [
+    255,
+    CH_VAL_MID,
+    CH_VAL_MID,
+    CH_VAL_MID,
+    CH_VAL_MID,
+    CH_VAL_MIDMIN,
+    CH_VAL_MIN,
+    CH_VAL_MAX,
+    CH_VAL_MIN,
+    CH_VAL_MIN,
+    CH_VAL_MIN,
+    CH_VAL_MIN,
+    CH_VAL_MIN,
+    CH_VAL_MIN,
+    CH_VAL_MIN,
+    CH_VAL_MIN,
+    CH_VAL_MIN,
+];
+
+let SbusDataGenerator = () => {
+    let neutralSbus = '';
+    for(let i in sbus_ch_val) {
+        neutralSbus += sbus_ch_val[i].toString(16);
+    }
+    neutralSbus += Calc_CRC_8(neutralSbus, sbus_ch_val.length).toString(16).padStart(2, '0');
+
+    Parse_RcData(neutralSbus);
+}
+
+let Parse_RcData = (rc_str) => {
+    let GCS_DataBuffer = Buffer.from(rc_str, 'hex');
+
+    SBUS1_CH[0] = GCS_DataBuffer[1] * CH_SCALE + 1;
+    SBUS1_CH[1] = GCS_DataBuffer[2] * CH_SCALE + 1;
+    SBUS1_CH[2] = GCS_DataBuffer[3] * CH_SCALE + 1;
+    SBUS1_CH[3] = GCS_DataBuffer[4] * CH_SCALE + 1;
+    SBUS1_CH[4] = GCS_DataBuffer[5] * CH_SCALE + 1;
+    SBUS1_CH[5] = GCS_DataBuffer[6] * CH_SCALE + 1;
+    SBUS1_CH[6] = GCS_DataBuffer[7] * CH_SCALE + 1;
+    SBUS1_CH[7] = GCS_DataBuffer[8] * CH_SCALE + 1;
+    SBUS1_CH[8] = GCS_DataBuffer[9] * CH_SCALE + 1;
+    SBUS1_CH[9] = GCS_DataBuffer[10] * CH_SCALE + 1;
+    SBUS1_CH[10] = GCS_DataBuffer[11] * CH_SCALE + 1;
+    SBUS1_CH[11] = GCS_DataBuffer[12] * CH_SCALE + 1;
+    SBUS1_CH[12] = GCS_DataBuffer[13] * CH_SCALE + 1;
+    SBUS1_CH[13] = GCS_DataBuffer[14] * CH_SCALE + 1;
+    SBUS1_CH[14] = GCS_DataBuffer[15] * CH_SCALE + 1;
+    SBUS1_CH[15] = GCS_DataBuffer[16] * CH_SCALE + 1;
+
+    sbus1Packet_Generator(SBUS1_CH);
+}
+
+let sbus1Packet_Generator = (SBUS_CH) => {
+    let SBUS_Buffer = [];
+    SBUS_Buffer.push(0x0f);
+    SBUS_Buffer.push((SBUS_CH[0] & 0x07FF));
+    SBUS_Buffer.push((SBUS_CH[0] & 0x07FF) >> 8 | (SBUS_CH[1] & 0x07FF) << 3);
+    SBUS_Buffer.push((SBUS_CH[1] & 0x07FF) >> 5 | (SBUS_CH[2] & 0x07FF) << 6);
+    SBUS_Buffer.push((SBUS_CH[2] & 0x07FF) >> 2);
+    SBUS_Buffer.push((SBUS_CH[2] & 0x07FF) >> 10 | (SBUS_CH[3] & 0x07FF) << 1);
+    SBUS_Buffer.push((SBUS_CH[3] & 0x07FF) >> 7 | (SBUS_CH[4] & 0x07FF) << 4);
+    SBUS_Buffer.push((SBUS_CH[4] & 0x07FF) >> 4 | (SBUS_CH[5] & 0x07FF) << 7);
+    SBUS_Buffer.push((SBUS_CH[5] & 0x07FF) >> 1);
+    SBUS_Buffer.push((SBUS_CH[5] & 0x07FF) >> 9 | (SBUS_CH[6] & 0x07FF) << 2);
+    SBUS_Buffer.push((SBUS_CH[6] & 0x07FF) >> 6 | (SBUS_CH[7] & 0x07FF) << 5);
+    SBUS_Buffer.push((SBUS_CH[7] & 0x07FF) >> 3);
+    SBUS_Buffer.push((SBUS_CH[8] & 0x07FF));
+    SBUS_Buffer.push((SBUS_CH[8] & 0x07FF) >> 8 | (SBUS_CH[9] & 0x07FF) << 3);
+    SBUS_Buffer.push((SBUS_CH[9] & 0x07FF) >> 5 | (SBUS_CH[10] & 0x07FF) << 6);
+    SBUS_Buffer.push((SBUS_CH[10] & 0x07FF) >> 2);
+    SBUS_Buffer.push((SBUS_CH[10] & 0x07FF) >> 10 | (SBUS_CH[11] & 0x07FF) << 1);
+    SBUS_Buffer.push((SBUS_CH[11] & 0x07FF) >> 7 | (SBUS_CH[12] & 0x07FF) << 4);
+    SBUS_Buffer.push((SBUS_CH[12] & 0x07FF) >> 4 | (SBUS_CH[13] & 0x07FF) << 7);
+    SBUS_Buffer.push((SBUS_CH[13] & 0x07FF) >> 1);
+    SBUS_Buffer.push((SBUS_CH[13] & 0x07FF) >> 9 | (SBUS_CH[14] & 0x07FF) << 2);
+    SBUS_Buffer.push((SBUS_CH[14] & 0x07FF) >> 6 | (SBUS_CH[15] & 0x07FF) << 5);
+    SBUS_Buffer.push((SBUS_CH[15] & 0x07FF) >> 3);
+
+    SBUS_Buffer.push(0x00);
+    SBUS_Buffer.push(0x00);
+
+    console.log('SBUS1_Buffer -\t\t', Buffer.from(SBUS_Buffer, 'hex'));
+
+    if (sbus1Port) {
+        sbus1Port.write(Buffer.from(SBUS_Buffer, 'hex'), () => {
+            // console.log(Buffer.from(SBUS1_Buffer, 'hex'));
+        });
+    }
+}
+
+setInterval(SbusDataGenerator, 30);
+
+exports.setDelta = (pan_diff_angle, tilt_diff_angle) => {
+    // pan
+    sbus_ch_val[4] = CH_VAL_MID + pan_diff_angle;
+    if(sbus_ch_val[4] >= CH_VAL_MAX) {
+        sbus_ch_val[4] = CH_VAL_MAX;
+    }
+    else if(sbus_ch_val[4] <= CH_VAL_MIN) {
+        sbus_ch_val[4] = CH_VAL_MIN;
+    }
+
+    // tilt
+    sbus_ch_val[2] = CH_VAL_MID + tilt_diff_angle;
+    if(sbus_ch_val[2] >= CH_VAL_MAX) {
+        sbus_ch_val[2] = CH_VAL_MAX;
+    }
+    else if(sbus_ch_val[2] <= CH_VAL_MIN) {
+        sbus_ch_val[2] = CH_VAL_MIN;
+    }
+}
+
+exports.setStop = () => {
+    sbus_ch_val[4] = CH_VAL_MID;
+    sbus_ch_val[2] = CH_VAL_MID;
+}
---
+++ tr_ctrl_bgc.js
@@ -0,0 +1,539 @@
+const mqtt = require('mqtt');
+const {nanoid} = require("nanoid");
+const fs = require("fs");
+
+const motor_bgc = require('./motor_bgc');
+
+const mavlink = require('./mavlibrary/mavlink.js');
+
+let tr_mqtt_client = null;
+
+let target_gpi = {};
+let tracker_control_message = '';
+let motor_altitude_message = '';
+let tracker_gpi = '';
+let tracker_att = '';
+let tracker_gri = '';
+
+let tracker_latitude = 37.4036621604629;
+let tracker_longitude = 127.16176249708046;
+let tracker_altitude = 0.0;
+let tracker_relative_altitude = 0.0;
+
+let tracker_fix_type = 0;
+let tracker_satellites_visible = 0;
+
+let tracker_roll = 0.0;
+let tracker_pitch = 0.0;
+let tracker_yaw = 0.0;
+
+let target_latitude = '';
+let target_longitude = '';
+let target_altitude = '';
+let target_relative_altitude = '';
+
+let gpsFlag = false;
+
+let drone_info = {};
+try {
+    drone_info = JSON.parse(fs.readFileSync('./drone_info.json', 'utf8'));
+}
+catch (e) {
+    console.log('can not find [ ./drone_info.json ] file');
+
+    drone_info.id = "Dione";
+    drone_info.approval_gcs = "MUV";
+    drone_info.host = "121.137.228.240";
+    drone_info.drone = "KETI_Simul_1";
+    drone_info.gcs = "KETI_GCS";
+    drone_info.type = "ardupilot";
+    drone_info.system_id = 1;
+    drone_info.gcs_ip = "192.168.1.150";
+
+    fs.writeFileSync('./drone_info.json', JSON.stringify(drone_info, null, 4), 'utf8');
+}
+
+let GcsName = drone_info.gcs;
+let DroneName = drone_info.drone;
+
+let dr_data_topic = '/Mobius/' + GcsName + '/Drone_Data/' + DroneName + '/#';
+
+let pn_ctrl_topic = '/Mobius/' + GcsName + '/Ctrl_Data/' + DroneName + '/Panel';
+let pn_offset_topic = '/Mobius/' + GcsName + '/Offset_Data/' + DroneName + '/Panel';
+let pn_alt_topic = '/Mobius/' + GcsName + '/Alt_Data/' + DroneName + '/Panel';
+let pn_gps_ctrl_topic = '/Mobius/' + GcsName + '/Gps_Ctrl_Data/' + DroneName + '/Panel';
+
+let tr_data_topic = '/Mobius/' + GcsName + '/Tr_Data/' + DroneName + '/PANTILT';
+
+let gps_pos_topic = '/Mobius/' + GcsName + '/Pos_Data/GPS';
+let gps_raw_topic = '/Mobius/' + GcsName + '/Gcs_Data/GPS';
+let gps_alt_topic = '/Mobius/' + GcsName + '/Att_Data/GPS';
+
+
+//------------- local mqtt connect ------------------
+function tr_mqtt_connect(host) {
+    let connectOptions = {
+        host: host,
+        port: 1883,
+        protocol: "mqtt",
+        keepalive: 10,
+        clientId: 'local_motor_can_' + nanoid(15),
+        protocolId: "MQTT",
+        protocolVersion: 4,
+        clean: true,
+        reconnectPeriod: 2 * 1000,
+        connectTimeout: 30 * 1000,
+        queueQoSZero: false,
+        rejectUnauthorized: false
+    }
+
+    tr_mqtt_client = mqtt.connect(connectOptions);
+
+    tr_mqtt_client.on('connect', () => {
+        console.log('tr_mqtt_client is connected to ' + host);
+
+        if (dr_data_topic !== '') {
+            tr_mqtt_client.subscribe(dr_data_topic, () => {
+                console.log('[tr_mqtt_client] sub_target_data_topic is subscribed -> ', dr_data_topic);
+            });
+        }
+
+        if (gps_alt_topic !== '') {
+            tr_mqtt_client.subscribe(gps_alt_topic, () => {
+                console.log('[tr_mqtt_client] gps_alt_topic is subscribed -> ', gps_alt_topic);
+            });
+        }
+
+        if (gps_pos_topic !== '') {
+            tr_mqtt_client.subscribe(gps_pos_topic, () => {
+                console.log('[tr_mqtt_client] gps_pos_topic is subscribed -> ', gps_pos_topic);
+            });
+        }
+
+        if (gps_raw_topic !== '') {
+            tr_mqtt_client.subscribe(gps_raw_topic, () => {
+                console.log('[tr_mqtt_client] gps_raw_topic is subscribed -> ', gps_raw_topic);
+            });
+        }
+
+        if (pn_ctrl_topic !== '') {
+            tr_mqtt_client.subscribe(pn_ctrl_topic, () => {
+                console.log('[tr_mqtt_client] pn_ctrl_topic is subscribed -> ', pn_ctrl_topic);
+            });
+        }
+
+        if (pn_offset_topic !== '') {
+            tr_mqtt_client.subscribe(pn_offset_topic, () => {
+                console.log('[tr_mqtt_client] pn_offset_topic is subscribed -> ', pn_offset_topic);
+            });
+        }
+
+        if (pn_gps_ctrl_topic !== '') {
+            tr_mqtt_client.subscribe(pn_gps_ctrl_topic, () => {
+                console.log('[tr_mqtt_client] pn_gps_ctrl_topic is subscribed -> ', pn_gps_ctrl_topic);
+            });
+        }
+
+        if (pn_alt_topic !== '') {
+            tr_mqtt_client.subscribe(pn_alt_topic, () => {
+                console.log('[tr_mqtt_client] pn_alt_topic is subscribed -> ', pn_alt_topic);
+            });
+        }
+    });
+
+    tr_mqtt_client.on('message', (topic, message) => {
+        let _dr_data_topic = dr_data_topic.replace('/#', '');
+        let arr_topic = topic.split('/');
+        let _topic = arr_topic.splice(0, arr_topic.length - 1).join('/');
+
+        if (topic === gps_pos_topic) { // 픽스호크로부터 받아오는 트래커 위치 좌표
+            tracker_gpi = JSON.parse(message.toString());
+
+            if (!gpsFlag) {
+                if (mavlink.GPS_FIX_TYPE_2D_FIX <= tracker_fix_type && tracker_fix_type <= mavlink.GPS_FIX_TYPE_DGPS) {
+                    tracker_latitude = tracker_gpi.lat / 10000000;
+                    tracker_longitude = tracker_gpi.lon / 10000000;
+                    tracker_altitude = tracker_gpi.alt / 1000;
+                    // tracker_altitude = tracker_gpi.relative_alt / 1000;
+                    tracker_relative_altitude = tracker_gpi.relative_alt / 1000;
+                }
+            }
+
+            countBPM++;
+        }
+        else if (topic === gps_raw_topic) { // 픽스호크로부터 받아오는 GPS 상태 정보
+            tracker_gri = JSON.parse(message.toString());
+
+            tracker_fix_type = tracker_gri.fix_type;
+            tracker_satellites_visible = tracker_gri.satellites_visible;
+
+            countBPM++;
+        }
+        else if (topic === gps_alt_topic) {
+            tracker_att = JSON.parse(message.toString());
+
+            tracker_yaw = ((tracker_att.yaw * 180) / Math.PI);
+            tracker_pitch = ((tracker_att.pitch * 180) / Math.PI);
+
+            countBPM++;
+        }
+        else if (topic === pn_ctrl_topic) { // 모터 제어 메세지 수신
+            tracker_control_message = message.toString();
+            tracker_handler(tracker_control_message);
+        }
+        else if (topic === pn_offset_topic) { // 모터 제어 메세지 수신
+            let offsetObj = JSON.parse(message.toString());
+            if (!offsetObj.hasOwnProperty('type')) {
+                p_offset = offsetObj.p_offset;
+                t_offset = offsetObj.t_offset;
+                console.log('[p_offset] -->', p_offset, '[t_offset] -->', t_offset);
+            }
+        }
+        else if (topic === pn_gps_ctrl_topic) { // 모터 제어 메세지 수신
+            if (message.toString() === 'release') {
+                gpsFlag = false;
+            }
+            else if (message.toString().includes('hold')) {
+                gpsFlag = true;
+                let msg_arr = message.toString().split(',');
+                tracker_altitude = msg_arr[1];
+            }
+        }
+        else if (topic === pn_alt_topic) {
+            motor_altitude_message = message.toString();
+            if (typeof (parseInt(motor_altitude_message)) === 'number') {
+                tracker_relative_altitude = motor_altitude_message;
+            }
+        }
+        else if (_topic === _dr_data_topic) { // 드론데이터 수신
+            if (flagTracking === 'yes') {
+                let arr_msg = message.toString().split(';');
+                if (arr_msg[0] === 'gpi') {
+                    target_gpi = JSON.parse(arr_msg[1]);
+
+                    target_latitude = target_gpi.lat / 10000000;
+                    target_longitude = target_gpi.lon / 10000000;
+                    target_altitude = target_gpi.alt / 1000;
+                    target_relative_altitude = target_gpi.relative_alt / 1000;
+
+                    //console.log('target_gpi: ', JSON.stringify(target_gpi));
+
+                    let tilt_t_angle = calcTargetTiltAngle(target_latitude, target_longitude, target_altitude);
+                    let pan_t_angle = calcTargetPanAngle(target_latitude, target_longitude);
+
+                    ctrlAngle(pan_t_angle, tilt_t_angle);
+                }
+            }
+        }
+    });
+
+    tr_mqtt_client.on('error', (err) => {
+        console.log('[tr_mqtt_client] error ' + err.message);
+    });
+}
+
+
+let tidControlTracker = null;
+let flagTracking = 'no';
+
+let tracker_handler = (_msg) => {
+    console.log('received message from panel', _msg);
+    if (_msg === 'arrange') {
+        stateCtrl = 'arranging';
+    }
+    else if (_msg === 'tilt_up') {
+        if (tidControlTracker) {
+            clearInterval(tidControlTracker);
+            tidControlTracker = null;
+        }
+
+        tidControlTracker = setInterval(() => {
+            motor_bgc.setDelta(0,1);
+        }, 100);
+    }
+    else if (_msg === 'tilt_down') {
+        if (tidControlTracker) {
+            clearInterval(tidControlTracker);
+            tidControlTracker = null;
+        }
+
+        tidControlTracker = setInterval(() => {
+            motor_bgc.setDelta(0,-1);
+        }, 100);
+    }
+    else if (_msg === 'pan_up') {
+        if (tidControlTracker) {
+            clearInterval(tidControlTracker);
+            tidControlTracker = null;
+        }
+
+        tidControlTracker = setInterval(() => {
+            motor_bgc.setDelta(1, 0);
+        }, 100);
+    }
+    else if (_msg === 'pan_down') {
+        if (tidControlTracker) {
+            clearInterval(tidControlTracker);
+            tidControlTracker = null;
+        }
+
+        tidControlTracker = setInterval(() => {
+            motor_bgc.setDelta(-1, 0);
+        }, 100);
+    }
+    else if (_msg === 'stop') {
+        if (tidControlTracker) {
+            clearInterval(tidControlTracker);
+            tidControlTracker = null;
+        }
+
+        motor_bgc.setStop();
+    }
+    else if (_msg === 'run') {
+        if (flagTracking === 'no') {
+            flagTracking = 'yes';
+        }
+        else {
+            flagTracking = 'no';
+        }
+    }
+}
+
+function calcTargetPanAngle(targetLatitude, targetLongitude) {
+    // let target_latitude_rad = targetLatitude * Math.PI / 180;
+    // let target_longitude_rad = targetLongitude * Math.PI / 180;
+    //
+    // let tracker_latitude_rad = tracker_latitude * Math.PI / 180;
+    // let tracker_longitude_rad = tracker_longitude * Math.PI / 180;
+    //
+    // let y = Math.sin(target_longitude_rad - tracker_longitude_rad) * Math.cos(target_latitude_rad);
+    // let x = Math.cos(tracker_latitude_rad) * Math.sin(target_latitude_rad) - Math.sin(tracker_latitude_rad) * Math.cos(target_latitude_rad) * Math.cos(target_longitude_rad - tracker_longitude_rad);
+    // let angle = Math.atan2(y, x); // azimuth angle (radians)
+
+    let cur_lat = tracker_latitude;
+    let cur_lon = tracker_longitude;
+
+    let result1 = dfs_xy_conv('toXY', cur_lat, cur_lon);
+
+    let tar_lat = targetLatitude;
+    let tar_lon = targetLongitude;
+
+    let result2 = dfs_xy_conv('toXY', tar_lat, tar_lon);
+
+    let x = result2.x - result1.x + 0.000001;
+    let y = result2.y - result1.y + 0.000001;
+
+    // console.log(cur_lat, cur_lon);
+    // console.log(tar_lat, tar_lon);
+    // console.log('x: ', x, '     y: ', y);
+
+    let angle = Math.atan2(y, x);
+    angle = -Math.round(angle * 180 / Math.PI) + 90;
+    return angle;
+}
+
+
+function getDistance(lat1, lon1, lat2, lon2) {
+    var radLat1 = Math.PI * lat1 / 180;
+    var radLat2 = Math.PI * lat2 / 180;
+    var theta = lon1 - lon2;
+    var radTheta = Math.PI * theta / 180;
+    var dist = Math.sin(radLat1) * Math.sin(radLat2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.cos(radTheta);
+    if (dist > 1)
+        dist = 1;
+
+    dist = Math.acos(dist);
+    dist = dist * 180 / Math.PI;
+    dist = dist * 60 * 1.1515 * 1.609344 * 1000;
+
+    return dist;
+}
+
+function calcTargetTiltAngle(targetLatitude, targetLongitude, targetAltitude) {
+    //let x = getDistance(tracker_latitude, tracker_longitude, targetLatitude, targetLongitude);
+
+    let cur_lat = tracker_latitude;
+    let cur_lon = tracker_longitude;
+    let cur_alt = tracker_altitude;
+
+    let result1 = dfs_xy_conv('toXY', cur_lat, cur_lon);
+
+    let tar_lat = targetLatitude;
+    let tar_lon = targetLongitude;
+    let tar_alt = targetAltitude;
+
+    let result2 = dfs_xy_conv('toXY', tar_lat, tar_lon);
+
+    let x = Math.sqrt(Math.pow(result2.x - result1.x, 2) + Math.pow(result2.y - result1.y, 2) + Math.pow((tar_alt - cur_alt), 2));
+
+    let y = targetAltitude - tracker_altitude;
+
+    // console.log(cur_lat, cur_lon, cur_alt);
+    // console.log(tar_lat, tar_lon, tar_alt);
+    // console.log('x: ', x, '     y: ', y);
+
+    let angle = Math.atan2(y, x);
+
+    return Math.round(angle * 180 / Math.PI);
+}
+
+const RE = 6371.00877; // 지구 반경(km)
+const GRID = 0.001; // 격자 간격(km)
+const SLAT1 = 30.0; // 투영 위도1(degree)
+const SLAT2 = 60.0; // 투영 위도2(degree)
+const OLON = 126.0; // 기준점 경도(degree)
+const OLAT = 38.0; // 기준점 위도(degree)
+const XO = 43; // 기준점 X좌표(GRID)
+const YO = 136; // 기1준점 Y좌표(GRID)
+
+function dfs_xy_conv(code, v1, v2) {
+    const DEGRAD = Math.PI / 180.0;
+    const RADDEG = 180.0 / Math.PI;
+
+    const re = RE / GRID;
+    const slat1 = SLAT1 * DEGRAD;
+    const slat2 = SLAT2 * DEGRAD;
+    const olon = OLON * DEGRAD;
+    const olat = OLAT * DEGRAD;
+
+    let sn = Math.tan(Math.PI * 0.25 + slat2 * 0.5) / Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+    sn = Math.log(Math.cos(slat1) / Math.cos(slat2)) / Math.log(sn);
+    let sf = Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+    sf = Math.pow(sf, sn) * Math.cos(slat1) / sn;
+    var ro = Math.tan(Math.PI * 0.25 + olat * 0.5);
+    ro = re * sf / Math.pow(ro, sn);
+    let rs = {};
+    if (code === "toXY") {
+        rs['lat'] = v1;
+        rs['lng'] = v2;
+        var ra = Math.tan(Math.PI * 0.25 + (v1) * DEGRAD * 0.5);
+        ra = re * sf / Math.pow(ra, sn);
+        var theta = v2 * DEGRAD - olon;
+        if (theta > Math.PI) theta -= 2.0 * Math.PI;
+        if (theta < -Math.PI) theta += 2.0 * Math.PI;
+        theta *= sn;
+        rs['x'] = Math.floor(ra * Math.sin(theta) + XO + 0.5);
+        rs['y'] = Math.floor(ro - ra * Math.cos(theta) + YO + 0.5);
+    }
+    else {
+        rs['x'] = v1;
+        rs['y'] = v2;
+        let xn = v1 - XO;
+        let yn = ro - v2 + YO;
+        ra = Math.sqrt(xn * xn + yn * yn);
+        if (sn < 0.0) -ra;
+        let alat = Math.pow((re * sf / ra), (1.0 / sn));
+        alat = 2.0 * Math.atan(alat) - Math.PI * 0.5;
+
+        if (Math.abs(xn) <= 0.0) {
+            theta = 0.0;
+        }
+        else {
+            if (Math.abs(yn) <= 0.0) {
+                theta = Math.PI * 0.5;
+                if (xn < 0.0) -theta;
+            }
+            else theta = Math.atan2(xn, yn);
+        }
+        let alon = theta / sn + olon;
+        rs['lat'] = alat * RADDEG;
+        rs['lng'] = alon * RADDEG;
+    }
+    return rs;
+}
+
+
+let countBPM = 0;
+let flagBPM = 0;
+setInterval(() => {
+    if (countBPM > 5) {
+        countBPM = 0;
+        flagBPM = 1;
+    }
+    else {
+        flagBPM = 0;
+    }
+}, 3000)
+
+tr_mqtt_connect('localhost');
+
+let pan_offset_ctrl = 0;
+let tilt_offset_ctrl = 0;
+let pan_diff_angle = 0;
+let tilt_diff_angle = 0;
+let p_offset = 0;
+let t_offset = 0;
+
+const DEG = 0.0174533;
+let ctrlAngle = (pan_t_angle, tilt_t_angle) => {
+
+    pan_offset_ctrl = tracker_yaw + p_offset;
+    pan_diff_angle = (pan_t_angle - pan_offset_ctrl);
+
+    tilt_offset_ctrl = tracker_pitch + t_offset;
+    tilt_diff_angle = (tilt_t_angle - tilt_offset_ctrl);
+
+    console.log('[diff_angle] -> ', pan_diff_angle, tilt_diff_angle);
+
+    if (Math.abs(pan_diff_angle) > 180) {
+        if (pan_diff_angle < 0) {
+            pan_diff_angle = pan_diff_angle + 360;
+        }
+        else {
+            pan_diff_angle = pan_diff_angle - 360;
+        }
+    }
+
+    if (Math.abs(tilt_diff_angle) > 180) {
+        if (tilt_diff_angle < 0) {
+            tilt_diff_angle = tilt_diff_angle + 360;
+        }
+        else {
+            tilt_diff_angle = tilt_diff_angle - 360;
+        }
+    }
+
+    motor_bgc.setStop();
+    motor_bgc.setDelta(pan_diff_angle, tilt_diff_angle);
+}
+
+let tr_heartbeat = {};
+let count_tr_heartbeat = 0;
+let watchdogCtrl = () => {
+    if (stateCtrl === 'ready') {
+        tr_heartbeat.pan_angle = tracker_yaw;
+        tr_heartbeat.tilt_angle = tracker_pitch;
+        tr_heartbeat.flag_tracking = flagTracking;
+        tr_heartbeat.state = stateCtrl;
+        tr_heartbeat.lat = tracker_latitude;
+        tr_heartbeat.lon = tracker_longitude;
+        tr_heartbeat.alt = tracker_altitude;
+        tr_heartbeat.relative_alt = tracker_relative_altitude;
+        tr_heartbeat.fix_type = tracker_fix_type;
+        if (tr_mqtt_client) {
+            tr_mqtt_client.publish(tr_data_topic, JSON.stringify(tr_heartbeat), () => {
+                console.log(tr_data_topic, JSON.stringify(tr_heartbeat));
+            });
+        }
+
+        setTimeout(watchdogCtrl, 1000);
+    }
+    else if (stateCtrl === 'arranging') {
+        if (flagBPM) {
+            setTimeout(() => {
+                ctrlAngle(0, 0);
+
+                stateCtrl = 'ready';
+                setTimeout(watchdogCtrl, 1000);
+            }, 1000);
+        }
+        else {
+            console.log('unknown My Position');
+            setTimeout(watchdogCtrl, 1000);
+        }
+    }
+}
+
+
+let stateCtrl = 'toReady'
+setTimeout(watchdogCtrl, 1000);
