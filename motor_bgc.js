4bf56d301a644e2b1229c6b3d5ff6647f6233bfe
gbsmfather <iyahn@keti.re.kr>
2023-10-16 17:41:38 +0900

    - 준비상태, 정렬상태, 트래킹상태로 동작 형태 정리

--- motor_bgc.js
+++ motor_bgc.js
@@ -122,16 +122,16 @@ let sbus_ch_val = [
 ];
 
 let SbusDataGenerator = () => {
-    //let neutralSbus = '';
-    // for(let i in sbus_ch_val) {
-    //     neutralSbus += sbus_ch_val[i].toString(16);
-    // }
+    let neutralSbus = '';
+    for(let i in sbus_ch_val) {
+        neutralSbus += sbus_ch_val[i].toString(16);
+    }
     //let neutralSbus = sbus_ch_val.toString(16);
-    //neutralSbus += Calc_CRC_8(neutralSbus, (sbus_ch_val.length)/2).toString(16).padStart(2, '0');
+    neutralSbus += Calc_CRC_8(neutralSbus, (sbus_ch_val.length)/2).toString(16).padStart(2, '0');
 
-    //console.log(neutralSbus);
+    console.log(neutralSbus);
 
-    let neutralSbus = 'ff7d7d7d7d191919191919191919191919';
+    //let neutralSbus = 'ff7d7d7d7d191919191919191919191919';
 
     Parse_RcData(neutralSbus);
 
