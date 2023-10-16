f35717ccfac0bda4ac5a6dfe827786f9ad3bb60e
gbsmfather <iyahn@keti.re.kr>
2023-10-16 21:54:33 +0900

    - SBUS 출력 확인

--- motor_bgc.js
+++ motor_bgc.js
@@ -193,7 +193,7 @@ let sbus1Packet_Generator = (SBUS_CH) => {
     SBUS_Buffer.push((SBUS_CH[14] & 0x07FF) >> 6 | (SBUS_CH[15] & 0x07FF) << 5);
     SBUS_Buffer.push((SBUS_CH[15] & 0x07FF) >> 3);
 
-    SBUS_Buffer.push(0x00);
+    //SBUS_Buffer.push(0x00);
     //SBUS_Buffer.push(0x00);
 
     //console.log('SBUS1_Buffer -\t\t', Buffer.from(SBUS_Buffer, 'hex'));
