316cdab90467139652d10ada58c0492e5b8d8cb7
dnjstjr93 <dnjstjr93@gmail.com>
2023-10-17 17:50:41 +0900

    - 연동 가능하도록 수정, tilt 계산식 수정

--- motor_bgc.js
+++ motor_bgc.js
@@ -9,13 +9,13 @@ let sbus_gen_tid = null;
 
 const CH_SCALE = 8;
 
-let SBUS1_CH = new Uint16Array([1024, 1024, 1024, 1024, 1024, 223, 223, 1024, 223, 223, 223, 1024, 223, 223, 223, 223, 223, 223]);
+let SBUS1_CH = new Uint16Array([1001, 1001, 1001, 1001, 201, 201, 201, 201, 201, 201, 369, 201, 201, 201, 201, 201, 223, 223]);
 
 const crc8_Table = [
     0, 94, 188, 226, 97, 63, 221, 131, 194, 156, 126, 32, 163, 253, 31, 65,  // 0 ~ 15
     157, 195, 33, 127, 252, 162, 64, 30, 95, 1, 227, 189, 62, 96, 130, 220,  // 16 ~ 31
     35, 125, 159, 193, 66, 28, 254, 160, 225, 191, 93, 3, 128, 222, 60, 98,  // 32 ~ 47
-    190, 224, 2, 92, 223, 129, 99, 61, 124, 34, 192, 158, 29, 67, 161, 255,		// 48 ~ 63
+    190, 224, 2, 92, 223, 129, 99, 61, 124, 34, 192, 158, 29, 67, 161, 255,             // 48 ~ 63
     70, 24, 250, 164, 39, 121, 155, 197, 132, 218, 56, 102, 229, 187, 89, 7,  // 64 ~ 79
     219, 133, 103, 57, 186, 228, 6, 88, 25, 71, 165, 251, 120, 38, 196, 154,  // 80 ~ 95
     101, 59, 217, 135, 4, 90, 184, 230, 167, 249, 27, 69, 198, 152, 122, 36,   // 96 ~ 111
@@ -52,28 +52,24 @@ let sbus1PortOpening = () => {
             setTimeout(sbus1PortOpening, 2000);
         });
 
-        sbus1Port.on('error', () => {
+        sbus1Port.on('error', (error) => {
             console.log('[sbus1Port error]: ' + error.message);
 
             setTimeout(sbus1PortOpening, 2000);
         });
 
-        // sbus1Port.on('data', (data) => {
-        //     //let _resData = data.toString('hex').toLowerCase();
-        //
-        //     //_resData = (sequence.toString(16).padStart(2, '0')) + _resData;
-        //
-        //     // if (dr_mqtt_client) {
-        //     //     dr_mqtt_client.publish(res_topic, Buffer.from(_resData, 'hex'));
-        //     // }
-        //     //
-        //     // if (mobius_mqtt_client) {
-        //     //     mobius_mqtt_client.publish(res_topic, Buffer.from(_resData, 'hex'));
-        //     // }
-        //
-        //     //sequence++;
-        //     //sequence %= 255;
-        // });
+// sbus1Port.on('data', (data) => {
+//     //let _resData = data.toString('hex').toLowerCase();
+//
+//     //_resData = (sequence.toString(16).padStart(2, '0')) + _resData;
+//
+//     // if (dr_mqtt_client) {
+//     //     dr_mqtt_client.publish(res_topic, Buffer.from(_resData, 'hex'));
+//     // }
+//     //
+//     // if (mobius_mqtt_client) {
+//     //     mobius_mqtt_client.publish(res_topic, Buffer.from(_resData, 'hex'));
+//     // }
     }
     else {
         if (sbus1Port.isOpen) {
@@ -101,39 +97,39 @@ let Calc_CRC_8 = (DataArray, Length) => {
 
 const CH_VAL_MAX = 225;
 const CH_VAL_MID = 128;
-const CH_VAL_MIDMIN = 56;
+const CH_VAL_MIDMIN = 46;
 const CH_VAL_MIN = 25;
 
 let sbus_ch_val = [
     255,
     CH_VAL_MID,
-    100,//CH_VAL_MID,
     CH_VAL_MID,
     CH_VAL_MID,
+    CH_VAL_MID,
+    58,
     CH_VAL_MIN,
     CH_VAL_MIN,
     CH_VAL_MIN,
-    CH_VAL_MIN,
-    CH_VAL_MIN,
-    CH_VAL_MIN,
-    CH_VAL_MIN,
-    CH_VAL_MIN,
-    CH_VAL_MIN,
-    CH_VAL_MIN,
-    CH_VAL_MIN,
-    CH_VAL_MIN,
+    CH_VAL_MID,
+    CH_VAL_MID,
+    CH_VAL_MIDMIN,
+    CH_VAL_MID,
+    CH_VAL_MID,
+    CH_VAL_MID,
+    CH_VAL_MID,
+    CH_VAL_MID,
 ];
 
 let SbusDataGenerator = () => {
-    let neutralSbus = '';
-    for(let i in sbus_ch_val) {
-        neutralSbus += sbus_ch_val[i].toString(16);
-    }
+    // let neutralSbus = '';
+    // for(let i in sbus_ch_val) {
+    //     neutralSbus += sbus_ch_val[i].toString(16);
+    // }
     //neutralSbus += Calc_CRC_8(neutralSbus, (sbus_ch_val.length)/2).toString(16).padStart(2, '0');
 
-    console.log(neutralSbus);
+    // console.log('sbus_ch_val', sbus_ch_val);
 
-    Parse_RcData(neutralSbus);
+    Parse_RcData(sbus_ch_val);
 
     //setTimeout(SbusDataGenerator, 30);
 }
@@ -141,8 +137,7 @@ let SbusDataGenerator = () => {
 let Parse_RcData = (rc_str) => {
     let GCS_DataBuffer = Buffer.from(rc_str, 'hex');
 
-    //let GCS_DataBuffer = rc_str;
-
+    console.log('GCS_DataBuffer', GCS_DataBuffer);
 
     SBUS1_CH[0] = GCS_DataBuffer[1] * CH_SCALE + 1;
     SBUS1_CH[1] = GCS_DataBuffer[2] * CH_SCALE + 1;
@@ -161,8 +156,7 @@ let Parse_RcData = (rc_str) => {
     SBUS1_CH[14] = GCS_DataBuffer[15] * CH_SCALE + 1;
     SBUS1_CH[15] = GCS_DataBuffer[16] * CH_SCALE + 1;
 
-    console.log(SBUS1_CH);
-
+    //console.log('SBUS1_CH', SBUS1_CH);
 
     sbus1Packet_Generator(SBUS1_CH);
 }
@@ -196,11 +190,9 @@ let sbus1Packet_Generator = (SBUS_CH) => {
     SBUS_Buffer.push(0x00);
     SBUS_Buffer.push(0x00);
 
-    //console.log('SBUS1_Buffer -\t\t', Buffer.from(SBUS_Buffer, 'hex'));
-
     if (sbus1Port) {
         sbus1Port.write(Buffer.from(SBUS_Buffer, 'hex'), () => {
-            // console.log(Buffer.from(SBUS1_Buffer, 'hex'));
+            console.log('SBUS_Buffer -\t\t', Buffer.from(SBUS_Buffer, 'hex'));
         });
     }
 }
@@ -216,7 +208,7 @@ exports.setDelta = (pan_diff_angle, tilt_diff_angle) => {
     }
 
     // tilt
-    sbus_ch_val[2] = CH_VAL_MID + tilt_diff_angle;
+    sbus_ch_val[2] = CH_VAL_MID - tilt_diff_angle;
     if(sbus_ch_val[2] >= CH_VAL_MAX) {
         sbus_ch_val[2] = CH_VAL_MAX;
     }
@@ -236,9 +228,9 @@ let init = () => {
         sbus_gen_tid = null;
     }
 
-    sbus_gen_tid = setInterval(SbusDataGenerator, 50);
+    sbus_gen_tid = setInterval(SbusDataGenerator, 15);
 }
 
 sbus1PortOpening();
 
-//setTimeout(SbusDataGenerator, 1000, sbus_ch_val);
+
