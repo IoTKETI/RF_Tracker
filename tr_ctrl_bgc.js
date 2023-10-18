d861c846cb452b711249610ad1492b0d12f87e6e
dnjstjr93 <dnjstjr93@gmail.com>
2023-10-18 16:49:45 +0900

    - ready일 때 현재 자세 유지하도록 수정

--- tr_ctrl_bgc.js
+++ tr_ctrl_bgc.js
@@ -241,7 +241,7 @@ function tr_mqtt_connect(host) {
 
 let tidControlTracker = null;
 let flagTracking = 'no';
-const STEP = 120;
+const STEP = 152;
 let tracker_handler = (_msg) => {
     console.log('received message from panel', _msg);
     if (_msg === 'arrange') {
